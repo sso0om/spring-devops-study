300b50f18daf8c38abaeeb586a56181ef4efb525
sso0om <ssooom142@gmail.com>
2025-08-31 22:34:53 +0900

    initial setting

---
+++ src/main/java/com/springdevopsstudy/SpringDevopsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.springdevopsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevopsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevopsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdevopsstudy/SpringDevopsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdevopsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevopsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
