5c71453abfe56e45bd25fcaee5b463df98e47e15
sso0om <ssooom142@gmail.com>
2025-09-01 20:49:53 +0900

    feat: homeContorller

---
+++ src/main/java/com/springdevopsstudy/HomeController.java
@@ -0,0 +1,12 @@
+package com.springdevopsstudy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello from EC2!";
+    }
+}
